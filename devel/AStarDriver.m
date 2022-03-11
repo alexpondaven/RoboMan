ede811a2e579452ac30f0d7db26d934a2847ecf0
tianyilim <0.tianyi.lim@gmail.com>
2022-03-11 13:45:32 +0000

    Add timing analysis to Astardriver

--- devel/AStarDriver.m
+++ devel/AStarDriver.m
@@ -28,16 +28,25 @@ cube_hold = [
 THETA_1_LIST = getOccupancyGridParams().THETA_1_LIST;
 ARM_END = getArmDimensions().L4;
 
+t1 = now;
 og = createOccupancyGrid(cube_locs, cube_hold);
+t2 = now;
+t_og = ((t2-t1)*24)*3600;
 
 disp('Created occupancy grid.')
 
 % TODO fix bugs in reachability analysis!!!
 
-startPos = [ 100, -100, 50, -pi/2 ];
-endPos = [ 200, 100, 50, 0 ];
+startPos = [ 225, 0, 15, -pi/2 ];
+endPos =   [ 225, 0, 15, 0 ];
 
+t1 = now;
 waypoints = AstarSearch( startPos, endPos, og );
+t2 = now;
+t_astar = ((t2-t1)*24)*3600;
+
+fprintf("Took %0.2fs to generate occupancy grid and %0.2fs to perform A* search.\n\n", t_og, t_astar);
+
 waypoints = [startPos; waypoints; endPos];    % add in the start and end poses!
 
 posPath = zeros([ size(waypoints,1) ,5]);
