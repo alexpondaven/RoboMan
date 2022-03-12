af89670f859bc9ea6deeffcf8043582e231f4b00
tianyilim <0.tianyi.lim@gmail.com>
2022-03-12 22:28:29 +0000

    Add sample quintic function

---
+++ src/interpolation/sampleQuinticVel.m
@@ -0,0 +1,20 @@
+function theta = sampleQuinticVel(coeffs, T, curTime)
+    % SampleQuinticVel Sample the velocity of the quintic interpolation at time `curTime`
+    % 
+    % ARGS
+    % coeffs    : Coefficients of quintic for each theta and segment
+    % T         : Time value for each via point
+    % curTime   : Time to sample the interpolation
+    %
+    % RETURN
+    % theta     : Theta values sampled at curTime
+    
+    % Detect which segment it is in
+    seg = find(T>curTime,1)-1;
+    dT = curTime - T(seg);
+    
+    % Sample cubic
+    row = 6*(seg-1)+1;
+    theta = coeffs(row+1,:) + 2*coeffs(row+2,:)*dT + 3*coeffs(row+3,:)*dT^2 ...
+            + 4*coeffs(row+3,:)*dT^3 + 5*coeffs(row+4,:)*dT^4;
+    end
