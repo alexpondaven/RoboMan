47d34ddb58e63ff01f5984cf9eedaeab3756cdbd
tianyilim <0.tianyi.lim@gmail.com>
2022-03-13 15:20:23 +0000

    cleanup formatting

--- src/interpolation/sampleQuintic.m
+++ src/interpolation/sampleQuintic.m
@@ -1,20 +1,20 @@
 function theta = sampleQuintic(coeffs, T, curTime)
-% SAMPLEQUINTIC   Sample the quintic interpolation at time `curTime`
-% 
-% ARGS
-% coeffs    : Coefficients of quintic for each theta and segment
-% T         : Time value for each via point
-% curTime   : Time to sample the interpolation
-%
-% RETURN
-% theta     : Theta values sampled at curTime
+    % SAMPLEQUINTIC   Sample the quintic interpolation at time `curTime`
+    % 
+    % ARGS
+    % coeffs    : Coefficients of quintic for each theta and segment
+    % T         : Time value for each via point
+    % curTime   : Time to sample the interpolation
+    %
+    % RETURN
+    % theta     : Theta values sampled at curTime
 
-% Detect which segment it is in
-seg = find(T>curTime,1)-1;
-dT = curTime - T(seg);
+    % Detect which segment it is in
+    seg = find(T>curTime,1)-1;
+    dT = curTime - T(seg);
 
-% Sample quintic
-row = 6*(seg-1)+1;
-theta = coeffs(row,:) + coeffs(row+1,:)*dT + coeffs(row+2,:)*dT^2 + coeffs(row+3,:)*dT^3 ...
-        + coeffs(row+3,:)*dT^4 + coeffs(row+4,:)*dT^5;
+    % Sample quintic
+    row = 6*(seg-1)+1;
+    theta = coeffs(row,:) + coeffs(row+1,:)*dT + coeffs(row+2,:)*dT^2 + coeffs(row+3,:)*dT^3 ...
+            + coeffs(row+3,:)*dT^4 + coeffs(row+4,:)*dT^5;
 end
