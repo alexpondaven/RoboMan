a571845ca0242909ed64155373ca1e8e98fc1b76
tianyilim <0.tianyi.lim@gmail.com>
2022-03-13 20:04:47 +0000

    add in small Ki term( to be tuned)

--- src/feedforwardPIcontrol.m
+++ src/feedforwardPIcontrol.m
@@ -10,10 +10,10 @@ function [jointVel, errorIntegral] = feedforwardPIcontrol(desiredVel, error, pre
 % jointVel      : output velocity terms
 % errorIntegral : summing prevError and error
 
-    % TODO Tune this, PI control is currently unused
+    % TODO Tune Ki / Kp
 
-    Kp = 1;
-    Ki = 0;     % Kp and Ki are scalars with values to be tuned
+    Kp = 1.25;
+    Ki = 0.1;     % Kp and Ki are scalars with values to be tuned
 
     errorIntegral = prevError + error;
     jointVel = desiredVel + Kp*error + Ki*errorIntegral;
