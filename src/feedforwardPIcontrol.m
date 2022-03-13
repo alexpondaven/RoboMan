294f1d11b00e988109d8b4043a7652eecefa2936
tianyilim <0.tianyi.lim@gmail.com>
2022-03-13 12:56:02 +0000

    Add PI control function prototype

---
+++ src/feedforwardPIcontrol.m
@@ -0,0 +1,19 @@
+function [jointVel, errorIntegral] = feedforwardPIcontrol(desiredVel, error, prevError)
+% PI+Feedforward controller
+% 
+% Args:
+% desiredVel : feedforward velocity term
+% error      : current joint error
+% prevError  : accumulated joint error for Integral term
+% 
+% Returns:
+% jointVel      : output velocity terms
+% errorIntegral : summing prevError and error
+
+    Kp = 1;
+    Ki = 1;     % Kp and Ki are scalars with values to be tuned
+
+    errorIntegral = prevError + error;
+    jointVel = desiredVel + Kp*error + Ki*errorIntegral;
+
+end
