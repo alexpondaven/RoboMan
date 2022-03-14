13a5c00ed9bf2667bbdeb5756a3df880984a4aad
tianyilim <0.tianyi.lim@gmail.com>
2022-03-14 21:52:42 +0000

    Fix bug in IKD2

--- src/inverseKinDynamixel2.m
+++ src/inverseKinDynamixel2.m
@@ -1,7 +1,7 @@
 function raw_angles = inverseKinDynamixel2(raw_angles)
 % Function to convert raw angles in radians to Dynamixel servo ticks.
 
-    joint_bounds = getServoLimits();
+    joint_bounds = getServoLimits();    % Expressed in ticks
 
     % Hip
     D11_PARAMS = struct('LB', joint_bounds(1,1), ...
@@ -38,20 +38,22 @@ function raw_angles = inverseKinDynamixel2(raw_angles)
         raw_angles(i) = raw_angles(i) * PARAMS(i).invert;     % invert if necessary (for shoulder, wrist, elbow)
         raw_angles(i) = raw_angles(i) + PARAMS(i).offset;
 
+        % Convert to ticks
+        raw_angles(i) = raw_angles(i) * 2048/pi;
+        raw_angles(i) = cast(raw_angles(i), 'uint32');
+
         % Make sure raw_angles is in range described by PARAMS
         if raw_angles(i) > PARAMS(i).UB
-            fprintf("Angle %04d of servo %d over upper bound\n", raw_angles(i), i)
+            fprintf("Angle %d of servo %d over upper bound %d\n", raw_angles(i), i, PARAMS(i).UB)
             
             raw_angles(i) = PARAMS(i).UB;
         end
         if raw_angles(i) < PARAMS(i).LB
-            fprintf("Angle %04d of servo %d under lower bound\n", raw_angles(i), i)
+            fprintf("Angle %d of servo %d under lower bound %d\n", raw_angles(i), i, PARAMS(i).LB)
             
             raw_angles(i) = PARAMS(i).LB;
         end
         
-        % Convert to ticks
-        raw_angles(i) = raw_angles(i) * 2048/pi;
     end
 
 end
