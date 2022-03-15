601d03f9dc8879faadb263e36b1557b04c23b6ef
tianyilim <0.tianyi.lim@gmail.com>
2022-03-15 20:06:59 +0000

    change initDynamixels to always set Servo 5 into position control mode

--- src/util/initDynamixels.m
+++ src/util/initDynamixels.m
@@ -28,8 +28,8 @@ function status = initDynamixels(port_num, mode)
     end
     MODE(5) = 3;    % always put servo 5 into position control
 
-    %% Write to servos
-    for i=1:length(params.DXL_LIST)
+    %% Write to servos 1-4 (position control)
+    for i=1:4
         % Disable Dynamixel torque to write settings to it
         write1ByteTxRx(port_num, params.PROTOCOL_VERSION, params.DXL_LIST(i), params.ADDR_PRO_TORQUE_ENABLE, 0);
 
@@ -39,7 +39,6 @@ function status = initDynamixels(port_num, mode)
         if mode=="vel"
             % Set Dynamixel Velocity PI gains
             write4ByteTxRx(port_num, params.PROTOCOL_VERSION, params.DXL_LIST(i), params.ADDR_VEL_P_GAIN, DYN_VEL_P);
-%             write4ByteTxRx(port_num, params.PROTOCOL_VERSION, params.DXL_LIST(i), params.ADDR_VEL_I_GAIN, DYN_VEL_I);
             
             % Set Dynamixel Velocity Limit
             write4ByteTxRx(port_num, params.PROTOCOL_VERSION, params.DXL_LIST(i), params.ADDR_VEL_LIMIT, velocityLimit);
@@ -78,12 +77,25 @@ function status = initDynamixels(port_num, mode)
         end
     end
 
+    %% Talk to Servo 5 (always position control mode)
+    write1ByteTxRx(port_num, params.PROTOCOL_VERSION, params.DXL_LIST(5), params.ADDR_PRO_TORQUE_ENABLE, 0);
+    write1ByteTxRx(port_num, params.PROTOCOL_VERSION, params.DXL_LIST(5), params.ADDR_PRO_OPERATING_MODE, 3);
+    write4ByteTxRx(port_num, params.PROTOCOL_VERSION, params.DXL_LIST(5), params.ADDR_MAX_POS, servoLimits(5,2));
+    write4ByteTxRx(port_num, params.PROTOCOL_VERSION, params.DXL_LIST(5), params.ADDR_MIN_POS, servoLimits(5,1));
+    write1ByteTxRx(port_num, params.PROTOCOL_VERSION, params.DXL_LIST(5), params.ADDR_PRO_TORQUE_ENABLE, 1);
+    dxl_comm_result = getLastTxRxResult(port_num, params.PROTOCOL_VERSION);
+    dxl_error = getLastRxPacketError(port_num, params.PROTOCOL_VERSION);
+    if dxl_comm_result ~= params.COMM_SUCCESS
+        fprintf('%s\n', getTxRxResult(params.PROTOCOL_VERSION, dxl_comm_result));
+    elseif dxl_error ~= 0
+        fprintf('%s\n', getRxPacketError(params.PROTOCOL_VERSION, dxl_error));
+    else
+        fprintf('Dynamixel %d has been successfully connected \n', 5);
+    end
     status = 0;
 
-    % Read from servos
-    for i=1:length(params.DXL_LIST)
-        % Disable Dynamixel torque to write settings to it
-
+    % Read from servos 1-4
+    for i=1:4
         % Put actuator into Position Control Mode
         ModeCheck = read1ByteTxRx(port_num, params.PROTOCOL_VERSION, params.DXL_LIST(i), params.ADDR_PRO_OPERATING_MODE);
         ModeCheck = ModeCheck==MODE(i);
@@ -96,9 +108,6 @@ function status = initDynamixels(port_num, mode)
             % Set Dynamixel Velocity PI gains
             velPCheck = read4ByteTxRx(port_num, params.PROTOCOL_VERSION, params.DXL_LIST(i), params.ADDR_VEL_P_GAIN);
             velPCheck = DYN_VEL_P==velPCheck;
-            velICheck = read4ByteTxRx(port_num, params.PROTOCOL_VERSION, params.DXL_LIST(i), params.ADDR_VEL_I_GAIN);
-%             velICheck = DYN_VEL_I==velICheck;
-            velICheck = 1;
 
             CheckArr = [ModeCheck 1 1 VelLimCheck velPCheck velICheck 1 1 1 1 1];
         else
@@ -133,7 +142,16 @@ function status = initDynamixels(port_num, mode)
             CheckArr    % Display error codes
             status = -1;    % error code
         end
+    end
 
+    %% Read from Servo 5
+    ModeCheck = 3==read1ByteTxRx(port_num, params.PROTOCOL_VERSION, params.DXL_LIST(5), params.ADDR_PRO_OPERATING_MODE);
+    PosUBCheck = servoLimits(5,2)==read4ByteTxRx(port_num, params.PROTOCOL_VERSION, params.DXL_LIST(5), params.ADDR_MAX_POS);
+    PosLBCheck = servoLimits(5,1)==read4ByteTxRx(port_num, params.PROTOCOL_VERSION, params.DXL_LIST(5), params.ADDR_MIN_POS);
+    if any([ModeCheck, PosUBCheck, PosLBCheck] == 0)
+        fprintf("Servo 5 values not written correctly.\n");
+        [ModeCheck, PosUBCheck, PosLBCheck]
+        status = -1;
     end
 
 end
