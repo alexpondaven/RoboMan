0b76066db9e9d30841c4aab2c828643a63dcbeb3
tianyilim <0.tianyi.lim@gmail.com>
2022-03-15 22:40:30 +0000

    adjust L4 to better describe robot geometry

--- src/consts/getArmDimensions.m
+++ src/consts/getArmDimensions.m
@@ -4,7 +4,7 @@ function dimensions = getArmDimensions()
     dimensions.L1 = 128;
     dimensions.L2 = 24;
     dimensions.L3 = 124;
-    dimensions.L4 = 126;           % Alter this parameter for end effector grip position
+    dimensions.L4 = 140;           % Alter this parameter for end effector grip position
     dimensions.alpha_3 = atan(dimensions.L2/dimensions.L1);
     dimensions.R_3 = sqrt(dimensions.L2^2 + dimensions.L1^2);
 end
