b348c9b1a8ed47ca6de56cfe6794cd7cf628461d
tianyilim <0.tianyi.lim@gmail.com>
2022-03-17 14:23:18 +0000

    Separate gains for two implementations of mainServoLoop

--- src/feedforwardPIcontrol2.m
+++ src/feedforwardPIcontrol2.m
@@ -1,4 +1,4 @@
-function [jointVel, errorIntegral] = feedforwardPIcontrol(desiredVel, error, prevError)
+function [jointVel, errorIntegral] = feedforwardPIcontrol2(desiredVel, error, prevError)
 % PI+Feedforward controller
 % 
 % Args:
@@ -12,10 +12,12 @@ function [jointVel, errorIntegral] = feedforwardPIcontrol(desiredVel, error, pre
 
     % TODO Tune Ki / Kp
 
-    Kp = 1.5;
-    Ki = 0.1;     % Kp and Ki are scalars with values to be tuned
+    Kp = 0.2;
+    Ki = 0.005;     % Kp and Ki are scalars with values to be tuned
+    Kd = 0.0;
 
     errorIntegral = prevError + error;
+    errorDerivative = 
     jointVel = desiredVel + Kp*error + Ki*errorIntegral;
 
 end
