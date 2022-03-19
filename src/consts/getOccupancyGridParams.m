0d81b456e8c86cf5c8a0ba56b5862a39f6320873
tianyilim <0.tianyi.lim@gmail.com>
2022-03-19 21:34:52 +0000

    tune occupancy grid vals

--- src/consts/getOccupancyGridParams.m
+++ src/consts/getOccupancyGridParams.m
@@ -5,7 +5,7 @@ function ogParams = getOccupancyGridParams()
     ogParams.CUBE_DIM = 25;             % in millimeters
     ogParams.HOLDER_DIM = 30;
     ogParams.HOLDER_OCCUPANCY_GRID_Z = 30;    % how big holder is in occupancy grid
-    ogParams.HOLDER_HEIGHT = 20;        % Top of the cube holder
+    ogParams.HOLDER_HEIGHT = 18;        % Top of the cube holder
     ogParams.HOVER_HEIGHT = 20;         % How high above cube stack to place end effector before pick/place
     ogParams.TOPGRAB_OFFSET = 6;        % Extra height to add when picking/grabbing from thetaG=-pi/2
     ogParams.xy_resolution = 12.5;
