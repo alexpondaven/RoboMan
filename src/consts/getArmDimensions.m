08b168a93a004eb2879d67a20555820f17ee4c56
tianyilim <0.tianyi.lim@gmail.com>
2022-03-20 18:36:41 +0000

    for pen holder dimensions

--- src/consts/getArmDimensions.m
+++ src/consts/getArmDimensions.m
@@ -4,9 +4,16 @@ function dimensions = getArmDimensions()
     dimensions.L1 = 128;
     dimensions.L2 = 24;
     dimensions.L3 = 124;
+    
+    %% For Task 2 Gripper
     dimensions.L4 = 75 + 22.5;        % Alter this parameter for end effector grip position (27.5)
                                     % 75 is the distance to the gripper adapter
     dimensions.L5 = 10;
+
+    %% For Task 3 Pen Holder
+    dimensions.L4 = 75 + 22.5;
+    dimensions.L5 = 0;
+
     dimensions.theta_go = atan2(dimensions.L5, dimensions.L4);
     dimensions.d_go = hypot(dimensions.L5, dimensions.L4);
     dimensions.alpha_3 = atan(dimensions.L2/dimensions.L1);
