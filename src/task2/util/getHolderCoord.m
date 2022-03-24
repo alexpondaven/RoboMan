be29cd1107d397240dcf1989e6842eac9bf2dd4f
alexpondaven <alex.pondaven@gmail.com>
2022-03-24 09:39:56 +0000

    Update getHolderCoord.m

--- src/task2/util/getHolderCoord.m
+++ src/task2/util/getHolderCoord.m
@@ -12,29 +12,54 @@ function [x,y,thetaG] = getHolderCoord(holderNum)
 % theta_g   : Orientation of end effector (radians)
 CUBE_SIZE = getOccupancyGridParams().CUBE_DIM;  % 25
 
+% DEMO DAY
 switch holderNum
     case 1
-        grid = [3,-8];
+        grid = [7,-7];
         thetaG = 0;
     case 2
-        grid = [9,0];
+        grid = [7,2];
         thetaG = 0;
     case 3
-        grid = [6,6];
+        grid = [0,7];
         thetaG = 0;
     case 4
-        grid = [5,-5];
-        thetaG = -pi/2;
+        grid = [0,-6];
+        thetaG = 0;
     case 5
-        grid = [4,0];
+        grid = [4,4];
         thetaG = -pi/2;
     case 6
-        grid = [0,4];
-        thetaG = -pi/2;
+        grid = [9,0];
+        thetaG = 0;
     otherwise
         disp("Invalid holder number - only 6 cube holders exist")
         return
 end
+% VIDEO
+% switch holderNum
+%     case 1
+%         grid = [3,-8];
+%         thetaG = 0;
+%     case 2
+%         grid = [9,0];
+%         thetaG = 0;
+%     case 3
+%         grid = [6,6];
+%         thetaG = 0;
+%     case 4
+%         grid = [5,-5];
+%         thetaG = -pi/2;
+%     case 5
+%         grid = [4,0];
+%         thetaG = -pi/2;
+%     case 6
+%         grid = [0,4];
+%         thetaG = -pi/2;
+%     otherwise
+%         disp("Invalid holder number - only 6 cube holders exist")
+%         return
+% end
 coord = grid * CUBE_SIZE;
 x = coord(1);
 y = coord(2);
